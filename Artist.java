7ce01e017ecf9521642f63a3b02aca9d7bf08ebd
Samuel Tallent <samuel.tallent00@gmail.com>
2021-05-05 22:20:09 -0400

    Create Artist.java

---
+++ Artist.java
@@ -0,0 +1,31 @@
+public class Artist {
+
+    private String mbid;
+    private String name;
+
+    public Artist() {
+
+    }
+
+    public Artist(String mb, String n) {
+        mbid = mb;
+        name = n;
+    }
+
+    public String getMbid() {
+        return mbid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMbid(String mbid) {
+        this.mbid = mbid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
